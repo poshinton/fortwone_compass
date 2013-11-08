75799312bebac1b9353a8feafe53a66c8498fb16
poshinton <464095417@qq.com>
2013-11-08 13:21:56 +0800

    first commit

---
+++ src/com/fortwone/compass/MainActivity.java
@@ -0,0 +1,276 @@
+package com.fortwone.compass;
+
+import java.util.Locale;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.location.LocationProvider;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+@SuppressLint("ResourceAsColor")
+public class MainActivity extends Activity {
+
+    private final float MAX_ROATE_DEGREE = 1.0f;
+    private SensorManager mSensorManager;
+    private Sensor mOrientationSensor;
+    private LocationManager mLocationManager;
+    private String mLocationProvider;
+    private float mDirection;
+    private float mTargetDirection;
+    private AccelerateInterpolator mInterpolator;
+    protected final Handler mHandler = new Handler();
+    private boolean mStopDrawing;
+    private boolean mChinease;
+    private TextView direct;
+    View mCompassView;
+    CompassView mPointer;
+    TextView mLocationTextView;
+    LinearLayout mDirectionLayout;
+    LinearLayout mAngleLayout;
+
+    protected Runnable mCompassViewUpdater = new Runnable() {
+        @Override
+        public void run() {
+            if (mPointer != null && !mStopDrawing) {
+                if (mDirection != mTargetDirection) {
+
+                    // calculate the short routine
+                    float to = mTargetDirection;
+                    if (to - mDirection > 180) {
+                        to -= 360;
+                    } else if (to - mDirection < -180) {
+                        to += 360;
+                    }
+
+                    // limit the max speed to MAX_ROTATE_DEGREE
+                    float distance = to - mDirection;
+                    if (Math.abs(distance) > MAX_ROATE_DEGREE) {
+                        distance = distance > 0 ? MAX_ROATE_DEGREE : (-1.0f * MAX_ROATE_DEGREE);
+                    }
+
+                    // need to slow down if the distance is short
+                    mDirection = normalizeDegree(mDirection
+                            + ((to - mDirection) * mInterpolator.getInterpolation(Math
+                                    .abs(distance) > MAX_ROATE_DEGREE ? 0.4f : 0.3f)));
+                    mPointer.updateDirection(mDirection);
+                }
+
+                updateDirection();
+
+                mHandler.postDelayed(mCompassViewUpdater, 20);
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initResources();
+        initServices();
+    }
+
+
+	@Override
+    protected void onResume() {
+        super.onResume();
+        if (mLocationProvider != null) {
+            updateLocation(mLocationManager.getLastKnownLocation(mLocationProvider));
+            mLocationManager.requestLocationUpdates(mLocationProvider, 2000, 10, mLocationListener);
+        } else {
+            mLocationTextView.setText(R.string.cannot_get_location);
+        }
+        if (mOrientationSensor != null) {
+            mSensorManager.registerListener(mOrientationSensorEventListener, mOrientationSensor,
+                    SensorManager.SENSOR_DELAY_GAME);
+        }
+        mStopDrawing = false;
+        mHandler.postDelayed(mCompassViewUpdater, 20);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mStopDrawing = true;
+        if (mOrientationSensor != null) {
+            mSensorManager.unregisterListener(mOrientationSensorEventListener);
+        }
+        if (mLocationProvider != null) {
+            mLocationManager.removeUpdates(mLocationListener);
+        }
+    }
+
+    private void initResources() {
+        mDirection = 0.0f;
+        mTargetDirection = 0.0f;
+        mInterpolator = new AccelerateInterpolator();
+        mStopDrawing = true;
+        mChinease = TextUtils.equals(Locale.getDefault().getLanguage(), "zh");
+
+        mCompassView = findViewById(R.id.view_compass);
+        mPointer = (CompassView) findViewById(R.id.compass_pointer);
+        mLocationTextView = (TextView) findViewById(R.id.textview_location);
+        mDirectionLayout = (LinearLayout) findViewById(R.id.layout_direction);
+        mAngleLayout = (LinearLayout) findViewById(R.id.layout_angle);
+
+        mPointer.setImageResource(mChinease ? R.drawable.compass_cn : R.drawable.compass);
+    }
+
+    @SuppressWarnings("deprecation")
+	private void initServices() {
+        // sensor manager
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mOrientationSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+
+        // location manager
+        mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+        criteria.setCostAllowed(true);
+        criteria.setPowerRequirement(Criteria.POWER_LOW);
+        mLocationProvider = mLocationManager.getBestProvider(criteria, true);
+
+    }
+
+	private void updateDirection() {
+        mDirectionLayout.removeAllViews();
+        mAngleLayout.removeAllViews();
+        direct=(TextView)findViewById(R.id.toward);
+        String t="";
+        float direction = normalizeDegree(mTargetDirection * -1.0f);
+        int yun=(int)direction;
+        
+        if (direction > 22.5f && direction < 157.5f) {
+            // east
+            if(mChinease && direction>22.5f && direction <67.5f){
+            	t="东北";
+            }
+            else if(direction > 112.5f && direction < 157.5f){
+            	t="东南";
+            }
+            else {
+            	t="东";
+            }
+            
+        }
+        
+        else if (direction > 202.5f && direction < 337.5f) {
+            // west
+            if(mChinease && direction>202.5f && direction <247.5f){
+            	t="西南";
+            }
+            else if(direction > 292.5f && direction < 337.5f){
+            	t="西北";
+            }
+            else {
+            	t="西";
+            }
+        }
+
+        if (direction > 157.5f && direction < 202.5f) {
+            // south
+            t="南";
+        } else if (direction < 22.5 || direction > 337.5f) {
+            // north
+            t="北";
+        }
+        direct.setText("您目前手机朝向：\n\n\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b"+t+"\b\b\b"+yun+"°");
+
+   
+	}
+
+    private void updateLocation(Location location) {
+    	
+        if (location == null) {
+        	
+            mLocationTextView.setText(R.string.getting_location);
+        } else {
+            StringBuilder sb = new StringBuilder();
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+            sb.append(mChinease ? "您现在位于 ：\n" : "you are locate in:");
+            if (latitude >= 0.0f) {
+                sb.append(getString(R.string.location_north, getLocationString(latitude)));
+            } else {
+                sb.append(getString(R.string.location_south, getLocationString(-1.0 * latitude)));
+            }
+
+            sb.append("\n");
+
+            if (longitude >= 0.0f) {
+                sb.append(getString(R.string.location_east, getLocationString(longitude)));
+            } else {
+                sb.append(getString(R.string.location_west, getLocationString(-1.0 * longitude)));
+            }
+
+            mLocationTextView.setText(sb.toString());
+        }
+    }
+
+    private String getLocationString(double input) {
+        int du = (int) input;
+        int fen = (((int) ((input - du) * 3600))) / 60;
+        int miao = (((int) ((input - du) * 3600))) % 60;
+        return String.valueOf(du) + "°" + String.valueOf(fen) + "'" + String.valueOf(miao) + "\"";
+    }
+
+    private SensorEventListener mOrientationSensorEventListener = new SensorEventListener() {
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            float direction = event.values[0] * -1.0f;
+            mTargetDirection = normalizeDegree(direction);
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    };
+
+    private float normalizeDegree(float degree) {
+        return (degree + 720) % 360;
+    }
+
+    LocationListener mLocationListener = new LocationListener() {
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            if (status != LocationProvider.OUT_OF_SERVICE) {
+                updateLocation(mLocationManager.getLastKnownLocation(mLocationProvider));
+            } else {
+                mLocationTextView.setText(R.string.cannot_get_location);
+            }
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+            updateLocation(location);
+        }
+
+    };
+}
